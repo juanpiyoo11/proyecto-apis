4de0bc5e35f9dd6358abb2e09131d00be6be33a1
serleo98 <33487419+serleo98@users.noreply.github.com>
2024-04-20 18:13:27 -0300

    Navbar (#1)
    * navbar versión 1
    
    primera version de la navbar
    
    * retoque en css
    
    elimine algunos comentarios ya que eran muchos y aparte cambie ciertos parametros
    
    ---------
    
    Co-authored-by: NachoLave <nacholavezzari@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './css/App.css'
-
+import Navbar from'./navbar'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Navbar /> 
     </>
   )
 }
