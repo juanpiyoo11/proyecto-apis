fcdd7b37cfce368e197ca76c118c673dec5898eb
OneSkanLeaf <lautibauza@gmail.com>
2024-04-25 19:28:51 -0300

    Agregado "React-router-dom" y tarjetas de productos

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './css/App.css'
-import Carrusel from './carrusel.jsx'
+import { useState } from "react";
+import "./css/App.css";
+import Carrusel from "./carrusel.jsx";
+import Navbar from "./navbar";
+import Productos from "./productos.jsx";
 
-import Navbar from'./navbar'
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <Navbar /> 
-
-      <Carrusel /> 
-
+      <Navbar />
+      <Carrusel />
+      <Productos />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
