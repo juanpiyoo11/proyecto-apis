cc313ea3dbdbc360e404a397ce79cb7e059ee558
serleo <serleo96.98@gmail.com>
2024-07-10 23:21:05 -0300

    add carrito redux

---
+++ src/redux/reducers/CarritoSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    items: [],
+};
+
+export const carritoSlice = createSlice({
+    name: 'cart',
+    initialState,
+    reducers: {
+        addItem: (state, action) => {
+            state.items.push(action.payload);
+        },
+        removeItem: (state, action) => {
+            state.items = state.items.filter((item) => item.id !== action.payload);
+        },
+        clearCart: (state) => {
+            state.items = [];
+        },
+    },
+});
+
+export const { addItem, removeItem, clearCart } = carritoSlice.actions;
+
+export default carritoSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { productReducer } from "./reducers/productSlice.js";
+import {carritoSlice} from "./reducers/CarritoSlice.js";
 
 export const store = configureStore({
   reducer: {
     products: productReducer,
+    cart: carritoSlice,
   },
 });
