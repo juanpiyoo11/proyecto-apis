715f02ee1ac5d013dd28e0e44f28948e58f7ed7c
NachoLave <110949438+NachoLave@users.noreply.github.com>
2024-07-12 17:27:51 -0300

    retoques

--- src/ProductsInfo.jsx
+++ src/ProductsInfo.jsx
@@ -7,7 +7,21 @@ import { useSelector } from 'react-redux';
 const Products = () => {
   const navigate = useNavigate();
   const [products, setProducts] = useState([]);
-  const token = useSelector(state => state.auth.token); // Obtén el token de autenticación desde Redux
+  const token = useSelector(state => state.auth.token); // Get authentication token from Redux
+
+  // Filter products based on search query
+  const [searchTerm, setSearchTerm] = useState("");
+  const filteredProducts = products.filter((product) =>
+    product.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    product.brand.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    product.category.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  // Filter products based on category
+  const [selectedCategory, setSelectedCategory] = useState("all");
+  const filteredProductsByCategory = filteredProducts.filter(
+    (product) => product.category === selectedCategory || selectedCategory === "all"
+  );
 
   useEffect(() => {
     if (token) {
@@ -15,7 +29,7 @@ const Products = () => {
         .then((data) => setProducts(data))
         .catch(error => console.error('Error al obtener productos:', error));
     }
-  }, [token]); // Ejecuta useEffect cuando cambie el token
+  }, [token]); // Run useEffect when token changes
 
   const deleteProductHandler = (id) => {
     try {
@@ -29,27 +43,30 @@ const Products = () => {
   };
 
   return (
-    <div>
-      <h1 className="title">Mis Productos</h1>
+    <div className="contenedor-todo">
+      <div>
+      
+      </div>
       <div className="productos">
-        {products.map((product) => (
-          <ul className="producto" key={product.id}>
-            <li>
-              <img src={product.image} alt="Imagen del producto" style={{ maxWidth: "200px", maxHeight: "200px" }} />
-            </li>
-            <li>Marca: {product.brand}</li>
-            <li>Nombre: {product.name}</li>
-            <li>Categoría: {product.category}</li>
-            <li>Precio: ${product.price}</li>
-            <li>Talle: {product.size}</li>
-            <li>Color: {product.color}</li>
-            <li>Sexo: {product.sex}</li>
-            <li>Stock: {product.stock}</li>
-            <li className="buttons">
+      
+        {filteredProductsByCategory.map((product) => (
+          <div className="producto" key={product.id}>
+            <img src={product.image} alt="Imagen del producto" style={{ maxWidth: "200px", maxHeight: "200px" }} />
+            <div className="producto-info">
+              <h2>{product.name}</h2>
+              <p>Marca: {product.brand}</p>
+              <p>Categoría: {product.category}</p>
+              <p>Precio: ${product.price}</p>
+              <p>Talle: {product.size}</p>
+              <p>Color: {product.color}</p>
+              <p>Sexo: {product.sex}</p>
+              <p>Stock: {product.stock}</p>
+            </div>
+            <div className="buttons">
               <button onClick={() => deleteProductHandler(product.id)}>Eliminar</button>
               <button onClick={() => navigate(`../modifyProducts/${product.id}`)}>Modificar</button>
-            </li>
-          </ul>
+            </div>
+          </div>
         ))}
       </div>
     </div>
@@ -57,3 +74,4 @@ const Products = () => {
 };
 
 export default Products;
+
